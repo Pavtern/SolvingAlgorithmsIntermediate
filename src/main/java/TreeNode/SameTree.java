665b35ceb35e93a7e258f994984cffd819e28afc
Student <ternavskii.pavel@gmail.com>
2024-10-24 10:48:00 +0200

    Leetcode 100. Same Tree (the final version)

--- src/main/java/TreeNode/SameTree.java
+++ src/main/java/TreeNode/SameTree.java
@@ -6,30 +6,21 @@ import java.util.Queue;
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
-        Queue<TreeNode> queueP = new LinkedList<>();
-        queueP.add(p);
-        Queue<TreeNode> queueQ = new LinkedList<>();
-        queueQ.add(q);
-        while (!queueQ.isEmpty()) {
-            TreeNode currentP = queueP.poll();
-            TreeNode currentQ = queueQ.poll();
-            if (currentP == null ^ currentQ == null)  return false;
-            if (currentP != null) {
-                if (currentP.val != currentQ.val) return false;
+        Queue<TreeNode[]> queue = new LinkedList<>();
+        queue.add(new TreeNode[]{p, q});
+        while (!queue.isEmpty()) {
+            TreeNode[] current = queue.poll();
+            TreeNode currentP = current[0];
+            TreeNode currentQ = current[1];
 
-                if (currentP.left == null ^ currentQ.left == null) return false;
-                if (currentP.left != null) {
-                    queueP.add(currentP.left);
-                    queueQ.add(currentQ.left);
-                }
+            if (currentP == null && currentQ == null) continue;
+            if (currentP == null || currentQ == null || currentP.val != currentQ.val) return false;
 
-                if (currentP.right == null ^ currentQ.right == null) return false;
-                if (currentP.right != null) {
-                    queueP.add(currentP.right);
-                    queueQ.add(currentQ.right);
-                }
-            }
+            queue.add(new TreeNode[]{currentP.left, currentQ.left});
+            queue.add(new TreeNode[]{currentP.right, currentQ.right});
+
         }
         return true;
     }
 }
+
